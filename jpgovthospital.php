9f5ad7494b9b993d1f97d52ab4b996803bb45019
drdrustidev-tech <65113493+drdrustidev-tech@users.noreply.github.com>
2025-10-16 23:31:28 +0530

    Update jpgovthospital.php

--- jpgovthospital.php
+++ jpgovthospital.php
@@ -1,8 +1,12 @@
+php artisan migrate:rollback --step=1 
+
 php artisan make:model Department -mcr
 short_name, medium_name, full_name
 
 php artisan make:model Designation -mcr
+short_name, medium_name, full_name
 
 php artisan make:model Opd -mcr
+short_name, medium_name, full_name, opd_no
 
 php artisan make:model PatientRegister -mcr
