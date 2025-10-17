6ee0671f42e0d86101b46c38e0b412b9a5a2beca
drdrustidev-tech <65113493+drdrustidev-tech@users.noreply.github.com>
2025-10-17 14:05:58 +0530

    Update jpgovthospital.php

--- jpgovthospital.php
+++ jpgovthospital.php
@@ -8,5 +8,13 @@ short_name, medium_name, full_name
 
 php artisan make:model Opd -mcr
 short_name, medium_name, full_name, opd_no
+php artisan make:model Gender -mcr
+short_name, medium_name, full_name
 
 php artisan make:model PatientRegister -mcr
+unique_id, opd_no, name, age, gender_id, address
+php artisan make:model YearlyOpdRegister -mcr
+patient_register_id, yearly_opd_register_date, opd_no
+
+php artisan make:model CentralOpdRegister -mcr
+patient_register_id, consult_date, old_new, opd_room_no
