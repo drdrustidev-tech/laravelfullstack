da21dc83fbdd2f424f3b018ebf0542513da07577
drdrustidev-tech <65113493+drdrustidev-tech@users.noreply.github.com>
2025-11-23 11:11:20 +0530

    Update jpgovtmysql.php

--- jpgovtmysql.php
+++ jpgovtmysql.php
@@ -6,6 +6,11 @@ WHERE prof_year_session_id = 12
 
 
 UPDATE prof_year_session_students
-SET start_date = '2025-01-01',
-    end_date = '2025-06-30'
+SET start_date = '2025-05-01',
+    end_date = '2026-09-30'
 WHERE student_id IN (101, 102, 103, 104);
+
+UPDATE prof_year_session_students
+SET start_date = '20244-11-01',
+    end_date = '2026-03-31'
+WHERE id BETWEEN 155 AND 222;
